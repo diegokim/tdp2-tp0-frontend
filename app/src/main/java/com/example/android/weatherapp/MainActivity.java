c3e7182e0380301674a48061cc73fd19322b7bca
diegokim <diegofk26@gmail.com>
2017-08-17 22:49:43 -0300

    ActionBar changed to Toolbar #15
    It is a more flexible alternative.
    Also the app now displays the title Nueva York as default.

--- app/src/main/java/com/example/android/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/android/weatherapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.android.weatherapp;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Menu menu;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.default_city);
+        toolbar.setTitleTextColor(0xFFFFFFFF);
     }
+
+
+
 }
